dad5e8a7529df7768576979955ee15398cad5f1a
Edward Pasenidis <me@edpasenidis.tech>
2020-04-12 22:15:11 +0300

    feat: add worldwide statistics function

--- src/cogs/main.py
+++ src/cogs/main.py
@@ -33,6 +33,31 @@ class Main(commands.Cog):
                 # url=f"https://www.countryflags.io/{request.get('countryInfo').get('iso2')}/shiny/64.png")
                 await ctx.channel.send(embed=embed)
 
+    @commands.command()
+    async def world(self, ctx):
+        print(f'Fetching worldwide stats')
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://corona.lmao.ninja/countries/World') as r:
+                request = await r.json()
+                embed = discord.Embed(
+                    title="COVID-19.Tracker", description="**Worldwide**", color=0x00ff00)
+                embed.add_field(
+                    name="Cases", value=request.get('cases'), inline=True)
+                embed.add_field(
+                    name="Today Cases", value=request.get('todayCases'), inline=True)
+                embed.add_field(
+                    name="Recovered", value=request.get('recovered'), inline=True)
+                embed.add_field(
+                    name="Critical", value=request.get('critical'), inline=True)
+                embed.add_field(
+                    name="Deaths", value=request.get('deaths'), inline=True)
+                embed.add_field(
+                    name="Today Deaths", value=request.get('todayDeaths'), inline=True)
+                embed.set_footer(text="https://covidtrack.tk")
+                embed.set_thumbnail(
+                    url="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/earth.png")
+                await ctx.channel.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Main(bot))
